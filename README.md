97ebd979a8797109dcb4fe98f35c89721824d540
David Jun <davj534@gmail.com>
2021-11-14 19:51:33 -0800

    part 1 completed. part 2 partially completed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 8 - Starter
+David Jun
+
+1. "Within a Github action that runs whenever code is pushed". The tests should be incremental, and you push code incrementally. The tests should be automated, so Github performs the action for you.
+2. no
+3. no. a message feature is made up of several components. Also, this feature is the main part of the user workflow.
+4. yes. this is an isolated feature. 
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -1,3 +1,5 @@
+// lab says to run with `npm run test` but can also use `npm test ./__tests__/lab8.test.js`
+
 describe('Basic user flow for Website', () => {
   // First, visit the lab 8 website
   beforeAll(async () => {
@@ -23,18 +25,19 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
-    // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
-    // Expect allArePopulated to still be true
-    expect(allArePopulated).toBe(true);
-
+    for(let i = 0; i < prodItems.length; i++){
+      console.log(`Checking product item ${i+1}/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItems[i].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+      // Expect allArePopulated to still be true
+      expect(allArePopulated).toBe(true);
+    }
     // TODO - Step 1
     // Right now this function is only checking the first <product-item> it found, make it so that
     // it checks every <product-item> it found
@@ -50,6 +53,13 @@ describe('Basic user flow for Website', () => {
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
     // Once you have the button, you can click it and check the innerText property of the button.
     // Once you have the innerText property, use innerText['_remoteObject'].value to get the text value of it
+    const prodItems = await page.$$('product-item');
+    const shadowRoot = await prodItems[0].getProperty('shadowRoot');
+    const addToCartBtn = await shadowRoot.$('button');
+    await addToCartBtn.click();
+    const innerText = await addToCartBtn.getProperty('innerText');
+    expect(innerText['_remoteObject'].value).toBe('Remove from Cart');
+
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
@@ -60,6 +70,16 @@ describe('Basic user flow for Website', () => {
     // Query select all of the <product-item> elements, then for every single product element
     // get the shadowRoot and query select the button inside, and click on it.
     // Check to see if the innerText of #cart-count is 20
+    const prodItems = await page.$$('product-item');
+    // for loop starts at i=1 since we already clicked the button of the 0th element in the previous step
+    for(let i = 1; i<prodItems.length; i++){
+      const shadowRoot = await prodItems[i].getProperty('shadowRoot');
+      const addToCartBtn = await shadowRoot.$('button');
+      await addToCartBtn.click();
+    }
+    const cartCount = await page.$('#cart-count');
+    const innerText = await cartCount.getProperty('innerText');
+    expect(innerText['_remoteObject'].value).toBe('20');
   }, 10000);
 
   // Check to make sure that after you reload the page it remembers all of the items in your cart
@@ -69,6 +89,20 @@ describe('Basic user flow for Website', () => {
     // Reload the page, then select all of the <product-item> elements, and check every
     // element to make sure that all of their buttons say "Remove from Cart".
     // Also check to make sure that #cart-count is still 20
+    
+    // source: https://stackoverflow.com/questions/55236975/how-to-reload-page-in-puppeteer
+    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+
+    const prodItems = await page.$$('product-item');
+    for(let i = 0; i<20; i++) {
+      const shadowRoot = await prodItems[i].getProperty('shadowRoot');
+      const addToCartBtn = await shadowRoot.$('button');
+      const innerText = await addToCartBtn.getProperty('innerText');
+      expect(innerText['_remoteObject'].value).toBe('Remove from Cart');
+    }
+    const cartCount = await page.$('#cart-count');
+    const innerText = await cartCount.getProperty('innerText');
+    expect(innerText['_remoteObject'].value).toBe('20');
   }, 10000);
 
   // Check to make sure that the cart in localStorage is what you expect
@@ -76,6 +110,10 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 5
     // At this point he item 'cart' in localStorage should be 
     // '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', check to make sure it is
+
+    // source: https://stackoverflow.com/questions/55725295/puppeteer-get-localstorage-from-a-website
+    const cart = await page.evaluate(() => localStorage.getItem("cart"));
+    expect(cart).toBe('[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]');
   });
 
   // Checking to make sure that if you remove all of the items from the cart that the cart
@@ -85,6 +123,16 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 6
     // Go through and click "Remove from Cart" on every single <product-item>, just like above.
     // Once you have, check to make sure that #cart-count is now 0
+
+    const prodItems = await page.$$('product-item');
+    for(let i = 0; i<prodItems.length; i++){
+      const shadowRoot = await prodItems[i].getProperty('shadowRoot');
+      const removeBtn = await shadowRoot.$('button');
+      await removeBtn.click();
+    }
+    const cartCount = await page.$('#cart-count');
+    const innerText = await cartCount.getProperty('innerText');
+    expect(innerText['_remoteObject'].value).toBe('0');
   }, 10000);
 
   // Checking to make sure that it remembers us removing everything from the cart
@@ -95,6 +143,20 @@ describe('Basic user flow for Website', () => {
     // Reload the page once more, then go through each <product-item> to make sure that it has remembered nothing
     // is in the cart - do this by checking the text on the buttons so that they should say "Add to Cart".
     // Also check to make sure that #cart-count is still 0
+
+    // source: https://stackoverflow.com/questions/55236975/how-to-reload-page-in-puppeteer
+    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+
+    const prodItems = await page.$$('product-item');
+    for(let i = 0; i<20; i++) {
+      const shadowRoot = await prodItems[i].getProperty('shadowRoot');
+      const addToCartBtn = await shadowRoot.$('button');
+      const innerText = await addToCartBtn.getProperty('innerText');
+      expect(innerText['_remoteObject'].value).toBe('Add to Cart');
+    }
+    const cartCount = await page.$('#cart-count');
+    const innerText = await cartCount.getProperty('innerText');
+    expect(innerText['_remoteObject'].value).toBe('0');
   }, 10000);
 
   // Checking to make sure that localStorage for the cart is as we'd expect for the
@@ -103,5 +165,8 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the localStorage...');
     // TODO - Step 8
     // At this point he item 'cart' in localStorage should be '[]', check to make sure it is
+    // source: https://stackoverflow.com/questions/55725295/puppeteer-get-localstorage-from-a-website
+    const cart = await page.evaluate(() => localStorage.getItem("cart"));
+    expect(cart).toBe('[]');
   });
 });
